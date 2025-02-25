0a14f3497ad383f3c414c2751770b9007e346416
ClintonOgembo <clintonogembo70@gmail.com>
2025-02-25 12:58:14 +0300

    done

--- DSA/bigOnotation.js
+++ DSA/bigOnotation.js
@@ -24,3 +24,26 @@ function printArray(arr) {
     }
 }
 printArray([1, 3, 5, 7]);
+
+// O(n^2) - Quadratic Time : using nested loops.
+function printPairs(arr) {
+    for (let i= 0; i < arr.length; i++) {
+        for (let j=0; j < arr.length; j++) {
+            console.log(arr[i],arr[j]); // runs 'n' * 'n' times
+        }
+    }
+}
+printPairs([9,8,7,6]);
+
+// O(log n) - Logarithmic Time : Binary search divides the problem in half each time.
+function binarySearch(arr, target) {
+    let left = 0, right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+console.log("Binary search: ",binarySearch([1,2,3,4],1));
