5cf757644f4f3bf415a8fcaddc392f3287f7c0ac
ClintonOgembo <clintonogembo70@gmail.com>
2025-03-03 22:28:41 +0300

    working on linkedlists

--- DSA/linkedlists.js
+++ DSA/linkedlists.js
@@ -14,10 +14,30 @@ class Linkedlist {
         const node = new Node(data);
 
         if(this.head === null) this.head = node;
-
+        else {
+            let current = this.head;
+            while(current.next != null) {
+                current = current.next;   
+            }
+            current.next = node;
+        }
+        this.length++;
+    }
+    contains(value){
+        let current = this.head;
+        while(current != null) {
+            if(current.data === value) return true
+            current = current.next;
+        }
+        return false;
     }
 }
 
 const list = new Linkedlist();
 list.append(5);
+list.append(10);
+list.append(20);
+list.append(30);
+console.log(list.contains(30)); // output: true
+console.log(list.contains(300));   // outputs: false
 console.log(list);
