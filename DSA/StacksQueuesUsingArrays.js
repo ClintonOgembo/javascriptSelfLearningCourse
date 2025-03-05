04295fd851b743abdff42dd2107627f41d9ab180
ClintonOgembo <clintonogembo70@gmail.com>
2025-03-05 22:49:30 +0300

    done using arrays to implement stacks and queues

--- DSA/StacksQueuesUsingArrays.js
+++ DSA/StacksQueuesUsingArrays.js
@@ -1,3 +1,5 @@
+// STACKS
+
 class Stack {
     constructor(){
         this.items = [];  // Using an array to store stack elements
@@ -36,7 +38,7 @@ class Stack {
     }
 }
 
-const stack = new Stack;
+const stack = new Stack();
 stack.push(2);
 stack.push(4);
 stack.push(6);
@@ -47,3 +49,58 @@ console.log("pop() removes: ", stack.pop());
 console.log(stack);
 console.log("top element is: ", stack.peek());
 console.log("size of the stack: ",stack.size());
+
+
+// QUEUES
+
+console.log("----------------------");
+console.log("QUEUES");
+console.log("----------------------");
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    // Enqueue - Add elements to the end
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    // Dequeue - remove the first element
+    dequeue() {
+        if(this.isEmpty()) return "Queue is empty";
+        return this.items.shift();
+    }
+
+    // Front - get the first element
+    front() {
+        if(this.isEmpty()) return "Queue is empty";
+        return this.items[0];
+    }
+
+    // check if the queue is empty
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    // Get the size of the queue
+    size() {
+        return this.items.length;
+    }
+
+    // print the queue
+    print() {
+        console.log("Final queue: ",this.items.toString());
+    }
+}
+
+// Testing the queue
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log("First element is: ", queue.front());
+console.log("dequeue removes: ", queue.dequeue());
+console.log("Size of the queue is: ", queue.size());
+queue.print();
